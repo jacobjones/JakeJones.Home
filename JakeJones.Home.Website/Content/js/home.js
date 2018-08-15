a2ed8f66096d9bafcc001ca5f9f59d397008826a
jacobjones <jacob.jones@valtech.com>
2018-08-14 22:11:03 -0400

    Adds the connection functionality

--- JakeJones.Home.Website/Content/js/home.js
+++ JakeJones.Home.Website/Content/js/home.js
@@ -1,5 +1,15 @@
 ﻿ready(function () {
 	if (pageIs("home")) {
+		var musicConnector = simpleConnect.connect("music-text", "current-album-wrapper", {
+			lineWidth: 2,
+			color: "#fff",
+			type: "horizontal",
+			offset: 20
+		});
+
+		document.getElementById('current-album-wrapper').sdrag(function () {
+			simpleConnect.repaintConnection(musicConnector);
+		});
 
 		ajax.get("/api/music/current", function (album) {
 			var e = document.getElementById("current-album");
@@ -10,7 +20,19 @@
 				e.src = album.imageUrl;
 				e.alt = album.title;
 				e.title = album.title;
+				simpleConnect.repaintConnection(musicConnector);
 			}
+		});
+
+		var bookConnector = simpleConnect.connect("book-text", "current-book-wrapper", {
+			lineWidth: 2,
+			color: "#fff",
+			type: "horizontal",
+			offset: 20
+		});
+
+		document.getElementById('current-book-wrapper').sdrag(function () {
+			simpleConnect.repaintConnection(bookConnector);
 		});
 
 		ajax.get("/api/books/current", function (book) {
@@ -22,8 +44,8 @@
 				e.src = book.imageUrl;
 				e.alt = book.title;
 				e.title = book.title;
+				simpleConnect.repaintConnection(bookConnector);
 			}
 		});
-
 	};
 });
