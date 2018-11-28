6410981828a653f7f84eecd49b238b931015ac36
jacobjones <jacob.jones@valtech.com>
2018-11-28 15:28:54 -0500

    Removes unnecessary image fetching from Discogs, improves track image quality

--- JakeJones.Home.Music/Models/Track.cs
+++ JakeJones.Home.Music/Models/Track.cs
@@ -2,17 +2,24 @@
 {
 	public class Track : ITrack
 	{
-		public Track(string artist, string title, string albumTitle, string albumMbid)
+		public Track(string artist, string title, string albumTitle)
 		{
 			Artist = artist;
 			Title = title;
 			AlbumTitle = albumTitle;
-			AlbumMbid = albumMbid;
+		}
+
+		public Track(string artist, string title, string albumTitle, string imageUrl)
+		{
+			Artist = artist;
+			Title = title;
+			AlbumTitle = albumTitle;
+			ImageUrl = imageUrl;
 		}
 
 		public string Artist { get; }
 		public string Title { get; }
 		public string AlbumTitle { get; }
-		public string AlbumMbid { get; }
+		public string ImageUrl { get; }
 	}
 }
