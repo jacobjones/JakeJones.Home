a0f6644ca159e02c46e4531da447deb53d906209
jacobjones <jacob.jones@valtech.com>
2018-11-29 14:05:15 -0500

    Adds image upload functionality

--- JakeJones.Home.Website/Content/js/admin/edit.js
+++ JakeJones.Home.Website/Content/js/admin/edit.js
@@ -8,12 +8,13 @@
 			toolbar1: 'codesample | formatselect | bold italic blockquote forecolor backcolor | imageupload link | alignleft aligncenter alignright  | numlist bullist outdent indent | fullscreen',
 			selection_toolbar: 'bold italic | quicklink h2 h3 blockquote',
 			autoresize_bottom_margin: 0,
-			paste_data_images: true,
+			paste_data_images: false,
 			image_advtab: true,
 			//file_picker_types: 'image',
 			relative_urls: false,
 			convert_urls: false,
-			branding: false
+			branding: false,
+			images_upload_url: "/blog/image"
 		});
-	};
+	}
 });
